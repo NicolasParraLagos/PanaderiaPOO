3793295609920e56fa800a606123f6aa4c1dc890
NicolasParra <nicolas.parralagos@gmail.com>
2020-12-13 08:50:20 -0300

    Segunda ventana
    Al darle al boton de ver clientes se abrira la ventana de clientes. De momento la puse con una tabal, pero si no cacho como manejarla la cambiare por una lista

--- Panaderia/src/Principal.java
+++ Panaderia/src/Principal.java
@@ -147,6 +147,9 @@ public class Principal extends javax.swing.JFrame {
         cmdClientes = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMaximumSize(new java.awt.Dimension(1143, 701));
+        setMinimumSize(new java.awt.Dimension(1143, 701));
+        setResizable(false);
 
         lblProducto.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         lblProducto.setText("PRODUCTO");
@@ -254,6 +257,11 @@ public class Principal extends javax.swing.JFrame {
         });
 
         cmdClientes.setText("Ver Clientes");
+        cmdClientes.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cmdClientesActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -572,6 +580,12 @@ public class Principal extends javax.swing.JFrame {
         txtDescripcion.setText(null);
         rbPendiente.setSelected(rootPaneCheckingEnabled);  
     }//GEN-LAST:event_cmdNuevoPedidoActionPerformed
+
+    private void cmdClientesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmdClientesActionPerformed
+        setVisible(false);
+        Clientes form = new Clientes();
+        form.setVisible(true);
+    }//GEN-LAST:event_cmdClientesActionPerformed
 
     /**
      * @param args the command line arguments
