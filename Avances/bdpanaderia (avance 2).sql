40c1d04d428f95d37285b8b3c9c0409c43d8fd57
NicolasParra <nicolas.parralagos@gmail.com>
2020-12-21 01:22:49 -0300

    La entrega Final

--- Avances/bdpanaderia (avance 2).sql
+++ Avances/bdpanaderia (avance 2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost:3306
--- Tiempo de generación: 21-12-2020 a las 04:20:26
+-- Tiempo de generación: 13-12-2020 a las 22:50:48
 -- Versión del servidor: 8.0.18
 -- Versión de PHP: 7.3.15
 
@@ -41,9 +41,9 @@ CREATE TABLE `clientes` (
 --
 
 INSERT INTO `clientes` (`rut`, `nombre`, `apellido`, `fono`, `direccion`) VALUES
-('14.421.123-3', 'Eric', 'Ramirez', '+56 9 4313 3333', 'Arlegui 852, Viña del Mar'),
+('14.421.123-3', 'Eric', 'Rodrigez', '+56 9 4313 3333', 'Arlegui 852, Viña del Mar'),
 ('20.174.520-9', 'Nicolas', 'Parra', '+56 9 8498 3577', 'Dresden 17, Villa Berlin, Valparaiso'),
-('21.007.423-6', 'Tito', 'Guana Bacoa', '+56 9 4444 4444', 'Av. Siempre Viva 123, Springfield');
+('21.007.423-6', 'Tito', 'Bacoa', '+56 9 3123 4122', 'Av. Siempre Viva 123, Springfield');
 
 -- --------------------------------------------------------
 
@@ -63,14 +63,12 @@ CREATE TABLE `pedidos` (
 --
 
 INSERT INTO `pedidos` (`numero`, `rutCliente`, `descripcion`, `estado`) VALUES
-(12, '20.174.520-9', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. \nVestibulum at egestas dui. Nulla nec', 'Cancelado'),
-(111, '20.174.520-9', 'Pellentesque at mi sit amet ex porttitor tincidunt ut lobortis mauris. Duis quis iaculis diam. Pelle', 'Cancelado'),
+(111, '20.174.520-9', 'oasomomas\nasomaosma', 'Cancelado'),
 (421, '14.421.123-3', 'qqqqqqqqqqqqqqqqqq sssssssssss\nasssss sadamsom  asmosmd\n\nasasmasmoamsoams osmdoamsdaosd', 'Listo'),
 (555, '14.421.123-3', 'omascomsamommocmoaoasc\nasmoasmsaasmoasmos\nasoasomasomsoas\nomomaasaaaaaaaaaaa', 'Pendiente'),
 (1243, '20.174.520-9', 'sadadadsadasdas', 'Listo'),
 (2144, '21.007.423-6', '---- uwu ----', 'Pendiente'),
 (2911, '21.007.423-6', 'osamosamo momasmosaoasm\n\noamsoamsa aomsodmosdmoasmdaso asmdo\nsomadoasmdoamso samdoamdsaomdoasmosm', 'Cancelado'),
-(5555, '20.174.520-9', '123456789\ndasasdomasdodmodasm\ndlmodqwdqw\ndodqnqwodqomdqod\nqdmqwdomqodwmoqmwdoqwm\nodsmdasodmomdsoasmd', 'Listo'),
 (33312, '20.174.520-9', 'MODIFICADO', 'Pendiente');
 
 -- --------------------------------------------------------
@@ -98,6 +96,7 @@ INSERT INTO `productos` (`codigo`, `nombre`, `tipo`, `precio`, `stock`, `stockCr
 ('87654321', 'Torta de Merengue', 'Pastel', 10000, 15, 5),
 ('asmoaa11', 'Tartaleta de Frutilla', 'Pastel', 7200, 20, 4),
 ('insa12nn', 'Marraqueta', 'Pan', 1300, 100, 30),
+('OM12msQQ', 'Pan de centeno', 'Pan', 2000, 15, 10),
 ('on12ee41', 'Hallulla', 'Pan', 900, 95, 32),
 ('PA1wsaMM', 'Pie de Limón', 'Pastel', 6000, 23, 4),
 ('PMVO1444', 'Pan de molde', 'Pan', 1700, 45, 12);
